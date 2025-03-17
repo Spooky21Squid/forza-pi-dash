1655d912910397f35c689a7d84a3b7458ac1716b
Mathew Fitzgerald <mathewfitzgerald00@gmail.com>
2025-03-17 13:39:57 +0000

    Add barebones single tire widget to paramWidgets

--- v2/src/ParamWidgets.py
+++ v2/src/ParamWidgets.py
@@ -1,6 +1,58 @@
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt, Slot
 from fdp import ForzaDataPacket
+from enum import Enum
+
+
+class SingleTireWidget(QtWidgets.QFrame):
+    """Represents a single tire temp/wear combo inside the large tire widget"""
+
+    # Which tire it represents on the car (eg. FL = front left)
+    class Corner(Enum):
+        FL = 0
+        FR = 1
+        RL = 2
+        RR = 3
+
+    def __init__(self, corner: Corner):
+        super().__init__()
+
+        self.corner = corner
+        
+        # Shows the temperature by changing the border colour of the box
+        self.tireIcon = QtWidgets.QFrame(frameShape=QtWidgets.QFrame.Box)
+        self.tireIcon.setObjectName("tire")
+
+        # Shows tire wear as a percentage
+        self.wear = QtWidgets.QLabel("0%")
+        self.wear.setAlignment(Qt.AlignCenter)
+
+        # Position the tire icon and wear % on different sides of the widget
+        # depending on orientation (left or right tire). Makes no difference
+        # to the widgets function.
+        layout = QtWidgets.QHBoxLayout(self)
+        if corner is self.Corner.FL or corner is self.Corner.RL:
+            layout.addWidget(self.wear)
+            layout.addWidget(self.tireIcon)
+        else:
+            layout.addWidget(self.tireIcon)
+            layout.addWidget(self.wear)
+        self.setLayout(layout)
+    
+    @Slot()
+    def update(self, fdp: ForzaDataPacket):
+        paramName = ""
+        if self.corner is self.Corner.FL:
+            paramName = "tire_wear_FL"
+        elif self.corner is self.Corner.FR:
+            paramName = "tire_wear_FR"
+        elif self.corner is self.Corner.RL:
+            paramName = "tire_wear_RL"
+        else:
+            paramName = "tire_wear_RR"
+        
+        # Update the tire wear and temperature with values from
+        # the fdp, and using temp values from dashConfig
 
 
 class TireSlipWidget(QtWidgets.QProgressBar):
